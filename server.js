ed36afb1bed799941d2cc7bfdedeab65ad95e0be
sammiefrog <sammiefrog@live.com>
2021-03-02 10:38:05 -0600

    adds basic file structure and an express server

---
+++ server.js
@@ -0,0 +1,27 @@
+// importing required dependencies
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv").config();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// Connect to the Mongo DB, handle depreciation warnings
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/pca-emptrack", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(console.log("Database is connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
