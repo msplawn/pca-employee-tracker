54816aaa0d736dedeb5b75089b84a5d9a9371228
msplawn <mmsplawn@gmail.com>
2021-03-09 19:26:18 -0600

    connected server & html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,20 @@
-// const path = require("path");
+const path = require("path");
+const express = require("express")
+// const app = express();
 // const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = (app) => {
 //USER HTML ROUTES FIRST
-//   app.get("/signup", function (req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/member");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/signup.html"));
-//   });
+  app.get("/", function(req, res) {
+      console.log(req);
+    res.sendFile(path.join(__dirname, "../public/html/login-page.html"));
+  });
 
-//   app.get("/login", (req, res) => {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/member");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/login.html"));
-//   });
+  app.get("/employee", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/employee-landing.html"));
+  });
 
-//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-//   app.get("/member", isAuthenticated, (req, res) => {
-//     res.sendFile(path.join(__dirname, "../public/member.html"));
-//   });
+  app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html/admin-landing.html"));
+  });
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const session = require("express-session");
 const flash = require("connect-flash");
 const logger = require("morgan");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
@@ -19,7 +20,9 @@ require("dotenv").config();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
+
+require("./routes/htmlRoutes")(app);
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
@@ -37,6 +40,12 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use(routes);
+
+
+
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./public/html/login-page.html"));
+// });
 
 
 // Connect to the Mongo DB, handle depreciation warnings
