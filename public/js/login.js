be76e98291d28b07d5d6fc7b209ce4accce055e8
msplawn <mmsplawn@gmail.com>
2021-10-19 20:52:31 -0500

    updated auth

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,28 @@
 // const bcrypt = require("bcrypt")
 
 $("#submit").on("click", () => {
-    const username = $("#username").val();
-    const password = $("#password").val();
+  const username = $("#username").val();
+  const password = $("#password").val();
 
-    const data = { username: username, password: password };
-    console.log(username, password);
+  const data = { username: username, password: password };
+  console.log(username, password);
 
 
-    fetch("/api/user/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify(data),
-    }).then(response => response.json()
-    ).then((res) => {
-      console.log(res);
-        window.location.href = "/employee";
-    });
+  fetch("/api/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data),
+  }
+  ).then(response => {
+    console.log(response)
+    response.json()
+  }
+  ).then((res) => {
+    console.log(res);
+    window.location.href = "/employee";
+  });
 
 })
